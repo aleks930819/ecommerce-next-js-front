a373971fd0d964ac2dfa6ac90fece08cc5f43797
Aleksandar Atanasov <107752460+aleks930819@users.noreply.github.com>
2023-09-22 12:21:56 +0300

    Update product-details-actions.tsx

--- components/products/product-details-actions.tsx
+++ components/products/product-details-actions.tsx
@@ -31,13 +31,13 @@ const ProductDetailsActions = ({
         className='px-8 py-2 gap-4
      dark:bg-gray-100 dark:text-gray-600
      dark:hover:bg-gray-300'
-        onClick={() => cart(product)}
+        onClick={() => cart(product as any)}
       >
         ADD TO CART
         <ShoppingCart size={24} />
       </Button>
       <Button
-        onClick={() => addItem(product)}
+        onClick={() => addItem(product as any)}
         className='cursor-pointer group bg-gray-400
    hover:bg-gray-400 dark:bg-gray-100 dark:hover:bg-gray-200
     text-center p-2 rounded-full '

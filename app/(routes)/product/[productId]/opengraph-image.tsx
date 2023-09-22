200c5ebf7361fd597d9fa30b7c437a8dbf0a6a52
Aleksandar Atanasov <aleks930819@gmail.com>
2023-09-22 20:05:45 +0300

    test opengraph-image.tsx

--- app/(routes)/product/[productId]/opengraph-image.tsx
+++ app/(routes)/product/[productId]/opengraph-image.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import { getProduct } from "@/actions/get-proudct";
+import { ImageResponse } from "next/server";
 
 export const size = {
   width: 1200,
@@ -8,8 +10,22 @@ export const size = {
 export const generateImage = async ({ params }: { params: any }) => {
   const product = await getProduct(params.productId);
 
-  return {
-    file: product?.images[0]?.url,
-    size,
-  };
+  return new ImageResponse(
+    (
+      <div tw='relative flex w-full h-full flex items-center justify-center'>
+        <div tw='aboslute flex inset-0'>
+          <img
+            src={product?.images[0]?.url + "&w=1200&h=630"}
+            alt={product?.name}
+            tw='flex flex-1'
+          />
+        </div>
+        {/* <div tw='flex flex-col tex'>
+          <h1 tw='text-white text-3xl font-bold'>{product?.name}</h1>
+          <p tw='text-white text-lg'>{product?.description}</p>
+        </div> */}
+      </div>
+    ),
+    size
+  );
 };
